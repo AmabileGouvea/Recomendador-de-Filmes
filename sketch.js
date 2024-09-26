20ce136f37773d374cc87698f065d07391799e58
Amabile Guntowski Gouvea <160881932+AmabileGouvea@users.noreply.github.com>
2024-09-26 09:24:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+// avatar, 14, ficção científica, ação
+// shang-chi e a lenda dos dez anéis, 12, ação, fantasia
+// alice no país das maravilhas, 10, infantil, aventura
+// piratas do caribe: navegando em águas misteriosas, 12, aventura, ação
+// harry potter e o prisioneiro de azkaban, 12, infantil, fantasia
+function setup() }
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
